535e483c21c20b5ddfa9afb0bb984480c3a42cef
simplon-marseille <acardnicolas@hotmail.com>
2019-05-13 17:20:16 +0200

    first commit

---
+++ fibonnacci.js
@@ -0,0 +1,7 @@
+//Suite de Fibonacci
+//Créez une fonction qui complète le tableau 'fibonacci' qui retourne
+//les n (avec n > 4) premiers éléments de la suite de Fibonacci.
+const fibonacci = [1, 2, 3, 5];
+const suiteDeFibonacci = (n) => {
+
+};
