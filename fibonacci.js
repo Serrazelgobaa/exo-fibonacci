f96c7c284a1cc795d79799fea9f9bbad8d45aae1
Tania <tania.juspin@gmail.com>
2019-06-06 12:38:44 +0200

    exo fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -2,6 +2,20 @@
 //Créez une fonction qui complète le tableau 'fibonacci' qui retourne
 //les n (avec n > 4) premiers éléments de la suite de Fibonacci.
 const fibonacci = [1, 2, 3, 5];
-const suiteDeFibonacci = (n) => {
 
+const suiteDeFibonacci = (n) => {
+	if(n<=4) {
+		return fibonacci;
+	}
+
+	else {
+		for(i=4;i<n;i++) {
+			resultat = fibonacci[i-1] + fibonacci[i-2];
+			fibonacci.push(resultat);
+		}
+	}
+
+	return fibonacci;
 };
+
+console.log(suiteDeFibonacci(14));
